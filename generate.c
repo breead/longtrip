0ae497df6743bc7859b6d9dbf3bdb298b9d410eb
Simon Ardfors <simon@ardfors.com>
2025-08-08 15:49:43 +0900

    Assignment should be done

--- generate.c
+++ generate.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "generate.h"
 
-// TODO: change to 64 bit ints?
 void generate_hotels(uint64_t *hotels, int n)
 {
+    srand(time(NULL));
     uint64_t current = 0;
     for (int i = 0; i < n; i++) {
-         hotels[i] = current;
-         current += 1 + rand() % 400 + 1;
+        hotels[i] = current;
+        // printf("hotels[%d] = %llu\n", i, hotels[i]);
+        current += 1 + rand() % 400 + 1;
     }
 }
--- longtrip.c
+++ longtrip.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <limits.h>
 #include <time.h>
 #include "generate.h"
@@ -7,15 +8,22 @@
 void longtrip(uint64_t *hotels, int nhotels, uint64_t *stops);
 void longtrip2(uint64_t *hotels, int nhotels, uint64_t *stops);
 
-int penalty(int dist) {
-    int diff = 200 - dist;
+uint64_t penalty(uint64_t dist) {
+    uint64_t diff = 200 - dist;
     return diff * diff;
 }
 
-void print_stops(uint64_t *hotels, uint64_t *stops, int nstops)
+void print_stops(uint64_t *hotels, int nstops, uint64_t *stops)
 {
     int printed = 0;
     int i;
+    if (nstops > 20) {
+        i = nstops-20;
+        printf("printing stops in last 20 hotels:\n");
+    }
+    else
+        printf("printing all stops:\n");
+
     for (i = nstops>20 ? nstops-20 : 0; i < nstops; i++) {
         if (stops[i]) {
             printf("stop at %d: %5llu ", i, hotels[i]);
@@ -29,15 +37,24 @@ void print_stops(uint64_t *hotels, uint64_t *stops, int nstops)
 
 int main()
 {
-    // int hotels_medium[] = {
+    /* testing can be done by uncommenting one hotels_test, setting nhotels
+     * correctly and then uncommenting section within #######*/
+    // uint64_t hotels_test[] = {
     //     0,286,454,718,1015,1192,1272,1408,1608,1802,
     //     1975,2245,2527,2674,2866,3027,3246,3544,3669,3868,
     //     3973,4115,4220,4483,4577,4805,5079,5213,5419    };
-    // int hotels_small[] = {0, 130, 211, 472, 863, 900};
+    // uint64_t hotels_test[] = {0, 130, 211, 472, 863, 900};
+    // uint64_t hotels_test[] = {0, 200, 400, 600, 800};
 
-    int nhotels = 10;
+    int nhotels = 25000;
+    printf("nhotels = %d\n", nhotels);
     uint64_t *hotels = malloc(sizeof(uint64_t) * nhotels);
     generate_hotels(hotels, nhotels);
+
+    /* uncomment when testing ############################# */
+    // for (int i = 0; i < nhotels; i++)
+    //     hotels[i] = hotels_test[i];
+    /* #################################################### */
 
     uint64_t *stops = malloc(sizeof(uint64_t) * nhotels);
     for (int i = 0; i < nhotels; i++)
@@ -46,19 +63,18 @@ int main()
     clock_t t0, t1;
     double elapsed_ms;
 
-    t0 = clock();
-    longtrip(hotels, nhotels, stops);
-    t1 = clock();
-    elapsed_ms = (double)(t1-t0) * 1000.0 / CLOCKS_PER_SEC;
-    printf("longtrip() took %.1f ms\n", elapsed_ms);
-
     // t0 = clock();
-    // longtrip2(hotels, nhotels, stops);
+    // longtrip(hotels, nhotels, stops);
     // t1 = clock();
     // elapsed_ms = (double)(t1-t0) * 1000.0 / CLOCKS_PER_SEC;
-    // printf("longtrip2() took %.1f ms\n", elapsed_ms);
+    // printf("longtrip() took %.1f ms\n", elapsed_ms);
 
-    // TODO: do I need to actually verify my solution somehow?
+    t0 = clock();
+    longtrip2(hotels, nhotels, stops);
+    t1 = clock();
+    elapsed_ms = (double)(t1-t0) * 1000.0 / CLOCKS_PER_SEC;
+    printf("longtrip2() took %.1f ms\n", elapsed_ms);
+
     uint64_t current = 0;
     uint64_t total = 0;
     for (int i = 0; i < nhotels; i++) {
@@ -68,9 +84,10 @@ int main()
             current = hotels[i];
         }
     }
-    print_stops(hotels, stops, nhotels);
+    print_stops(hotels, nhotels, stops);
     printf("optimal penalty: %llu\n", total);
     free(hotels);
+    free(stops);
 }
 
 /* longtrip: dumbly solve assignment 6.2
@@ -92,7 +109,7 @@ void longtrip(uint64_t *hotels, int nhotels, uint64_t *stops)
 
         for (int i = 0; i < nhotels; i++) {
             if (mask & (1ULL << i)) {
-                int dist = hotels[i] - current;
+                uint64_t dist = hotels[i] - current;
 
                 total += penalty(dist);
                 current = hotels[i];
@@ -113,7 +130,7 @@ void longtrip2(uint64_t hotels[], int nhotels, uint64_t stops[])
     uint64_t d[nhotels];
     uint64_t prev[nhotels]; // previous stop in optimal path
     d[0] = hotels[0]; // hotels[0] should be 0
-    prev[0] = -1;
+    prev[0] = 0;
 
     for (int j = 1; j < nhotels; j++) {
         uint64_t min_penalty = UINT64_MAX;
@@ -132,7 +149,8 @@ void longtrip2(uint64_t hotels[], int nhotels, uint64_t stops[])
 
     // TODO: disable/change for large tests?
     // for (int i = 0; i < nhotels; i++)
-    //     printf("d[%d] = %d, prev[%d] = %d\n", i, d[i], i, prev[i]);
+    //     printf("d[%d] = %llu, prev[%d] = %llu\n", i, d[i], i, prev[i]);
+
     for (int i = 0; i < nhotels; i++)
         stops[i] = 0;
     for (int j = nhotels - 1; j > 0; j = prev[j])
